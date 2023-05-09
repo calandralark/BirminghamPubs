f689d2eaaee6c5d52ec9c41b811445fd8540047a
apa999 <apa999@me.com>
2023-05-09 15:37:30 +0100

    Initial Commit

---
+++ BirminghamPubs/BirminghamPubsApp.swift
@@ -0,0 +1,17 @@
+//
+//  BirminghamPubsApp.swift
+//  BirminghamPubs
+//
+//  Created by Anthony Abbott on 09/05/2023.
+//
+
+import SwiftUI
+
+@main
+struct BirminghamPubsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
